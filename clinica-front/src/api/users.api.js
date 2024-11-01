13f655b54aec2c1a1ec92b8e797dd69be4471be4
Nicroft <ortiznicolas28@gmail.com>
2024-11-01 14:09:46 -0500

    apis fix

--- clinica-front/src/api/users.api.js
+++ clinica-front/src/api/users.api.js
@@ -4,7 +4,6 @@ const usersApi = axios.create({
     baseURL: 'http://localhost:8000/api/User/'
 });
 
-// Obtener todos los doctores
 export const getAllDoctors = () => {
     return usersApi.get("/").then(response => {
         return response.data.filter(user => user.role === "Médico");
